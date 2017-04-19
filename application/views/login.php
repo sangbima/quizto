cabfd1380492b68f51c007e6d9669556e84fc2dc
Khairil <sangbima.net@gmail.com>
2017-04-19 13:11:59 +0700

    tambah batas waktu registrasi

--- application/views/login.php
+++ application/views/login.php
@@ -20,7 +20,14 @@
                         <input class="form-control" type="password" name="password" required="" placeholder="<?php echo $this->lang->line('password');?>" id="inputPassword">
                         <div class="checkbox"></div>
                         <button class="btn btn-primary btn-block btn-lg btn-signin" type="submit"><?php echo $this->lang->line('login');?></button>
+                        <?php
+                            $now = date('Y-m-d H:i:s', time());
+                            if($now < date($this->config->item('batas_pendaftaran'))) {
+                        ?>
                         <a class="btn btn-primary btn-block btn-lg btn-reg" href="register">Daftar</a>
+                        <?php
+                            }
+                        ?>
                     </form>
                 </div>
             </div>
